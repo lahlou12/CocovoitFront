4b00a492465f3657f8cd00a4011a79545c216c05
AmineKARA <aminekaramos@gmail.com>
2020-01-19 18:16:52 +0100

    update repo

--- profil.js
+++ profil.js
@@ -1,3 +1,5 @@
+
+
 async function delete_account(){
 
   var id_user = "5e2457050b4c16b5b368c6a2"
@@ -10,13 +12,8 @@ async function delete_account(){
       headers: {'Content-Type': 'application/json'},
   };
 
-  console.log(data)
-  alert("2");
-     const  response = await fetch("http://localhost:3234/users/login/"+id_user, requestOptions)
-     alert("3")
+     const  response = await fetch("http://localhost:3234/users/:"+id_user, requestOptions)
       const json = await response.json();
-      alert("4")
       console.log(json);
-      alert("5");
       
       }
