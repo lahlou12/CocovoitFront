5a7b7f0c4bb880c3f56a883c4141e34b63fa157d
AmineKARA <aminekaramos@gmail.com>
2020-01-20 17:50:54 +0100

    update front

--- recherche.js
+++ recherche.js
@@ -1,33 +1,10 @@
+  function lolo(){
+
+    console.log("OK");
+
+  }
+
 function recherche(){
-
-  // var form_mail = document.getElementById('email').value;
-  // var form_pass = document.getElementById('pass').value;
-  // var test = "ok";
-
-  //     // alert(form_pass);
-  //     // alert(form_mail);
-  //     alert("1");
-
-  // data = {
-  //   mail: form_mail,
-  //   motdepasse: form_pass,
-  // }
-
-  // const requestOptions = {
-  //     method: 'POST',
-  //     mode: "cors",
-  //     body: JSON.stringify(data),
-  //     headers: {'Content-Type': 'application/json'},
-  // };
-
-  // console.log(data)
-  // alert("2");
-  // const  response = await (fetch("http://localhost:3234/users/login/", requestOptions))
-  // alert("3")
-  // const json = await response.json();
-  // alert("4")
-  // console.log(json);
-  // alert("5");
 
 var form_start = document.getElementById('add_start').value;
 var form_end = document.getElementById('add_end').value;
@@ -52,5 +29,7 @@ var form_end = document.getElementById('add_end').value;
       .catch((error)=>{
         console.log(error)
       })
-    }
-  
+}
+
+
+
