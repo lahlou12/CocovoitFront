0552124b57b2f4c5ae22b9736843d9013863ca0e
AmineKARA <aminekaramos@gmail.com>
2020-01-21 02:57:36 +0100

    profil

--- js/connect.js
+++ js/connect.js
@@ -1,33 +1,5 @@
 function connect(){
 
-  // var form_mail = document.getElementById('email').value;
-  // var form_pass = document.getElementById('pass').value;
-  // var test = "ok";
-
-  //     // alert(form_pass);
-  //     // alert(form_mail);
-  //     alert("1");
-
-  // data = {
-  //   mail: form_mail,
-  //   motdepasse: form_pass,
-  // }
-
-  // const requestOptions = {
-  //     method: 'POST',
-  //     mode: "cors",
-  //     body: JSON.stringify(data),
-  //     headers: {'Content-Type': 'application/json'},
-  // };
-
-  // console.log(data)
-  // alert("2");
-  // const  response = await (fetch("http://localhost:3234/users/login/", requestOptions))
-  // alert("3")
-  // const json = await response.json();
-  // alert("4")
-  // console.log(json);
-  // alert("5");
 
 var form_mail = document.getElementById('email').value;
 var form_pass = document.getElementById('pass').value;
@@ -53,10 +25,12 @@ const data = {
       .then((res)=>{
           console.log(res)
           localStorage.setItem('user_id', res.user_id);
+          window.location.replace("../html/index.html");
       })
 
       .catch((error)=>{
-        console.log(error)
+        
+        console.log(reject)
       })
     }
   
--- js/profil.js
+++ js/profil.js
@@ -1,19 +1,107 @@
 
+function load_user(){
 
-async function delete_account(){
+  var id_user = localStorage.getItem('user_id');
 
-  var id_user = "5e2457050b4c16b5b368c6a2"
-  var test = "ok";
-
-
-  const requestOptions = {
-      method: 'DELETE',
-      mode: "cors",
-      headers: {'Content-Type': 'application/json'},
-  };
-
-     const  response = await fetch("http://localhost:3000/users/:"+id_user, requestOptions)
-      const json = await response.json();
-      console.log(json);
       
-      }
+      fetch('http://localhost:3000/users/'+id_user,{
+        method: 'GET',
+        mode: "cors",
+        headers: {"Content-Type": "application/json"}
+      })
+      .then((res)=>{
+        return new Promise((resolve,reject)=>{
+            resolve (res.json())
+        })
+      })
+
+      .then((res)=>{
+          //console.log(res.user);
+          localStorage.setItem('infos_user', JSON.stringify(res.user));
+      })
+
+      .catch((error)=>{
+        console.log(error)
+      })
+
+}
+
+
+function modifier_profil(){
+
+      id = localStorage.getItem('user_id');
+
+      var form_mail = document.getElementById('email').value;
+      var form_name = document.getElementById('name').value;
+      var form_prenom = document.getElementById('prenom').value;
+      var form_birth = document.getElementById('birth').value;
+      var form_pass = document.getElementById('pass').value;
+      var form_pass2 = document.getElementById('pass2').value;
+      var form_immat = document.getElementById('immat').value;
+      var form_modelV = document.getElementById('modelV').value;
+      var form_bio = document.getElementById('bio').value;
+
+  data = {
+    name: form_name,
+    mail: form_mail,
+    prenom: form_prenom,
+    dateNaissance: form_birth,
+    motDePasse: form_pass,
+    matricule: form_immat,
+    modelVoiture: form_modelV,
+    biographie: form_bio
+  }
+
+      fetch('http://localhost:3000/'+id,{
+        method: 'PUT',
+        mode: "cors",
+        body:JSON.stringify(data),
+        headers: {"Content-Type": "application/json"}
+      })
+      .then((res)=>{
+        return new Promise((resolve,reject)=>{
+            resolve (res.json())
+        })
+      })
+
+      .then((res)=>{
+          console.log(res);
+          //document.location.reload(true);
+      })
+
+      .catch((error)=>{
+        console.log(error)
+      })
+
+
+
+
+}
+
+function delete_account(){
+
+
+var id_user = localStorage.getItem('user_id');
+
+      
+      fetch('http://localhost:3000/users/'+id_user,{
+        method: 'DELETE',
+        mode: "cors",
+        headers: {"Content-Type": "application/json"}
+      })
+      .then((res)=>{
+        return new Promise((resolve,reject)=>{
+            resolve (res.json())
+        })
+      })
+
+      .then((res)=>{
+          console.log(res)
+      })
+
+      .catch((error)=>{
+        
+        console.log(reject)
+      })
+    }
+  
